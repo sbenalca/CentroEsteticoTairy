7f8e1f11e1532e94fe175461d0e981f102747678
Nicole Agila <nagila@espol.edu.ec>
2021-01-19 15:26:15 -0500

    endpoints comentarios, integrantes y tratamientos

---
+++ full-project/CET-backend/controllers/cliente.controller.js
@@ -0,0 +1,15 @@
+comentarios = require("../resources/comments.json");
+tratamientos = require("../resources/tratamientos.json");
+integrantes = require("../resources/integrantes.json");
+
+exports.allComentarios = (req, res) =>{
+    res.send(comentarios)
+}
+
+exports.allTratamientos = (req, res) =>{
+    res.send(tratamientos)
+}
+
+exports.allIntegrantes = (req, res) =>{
+    res.send(integrantes)
+}
--- full-project/CET-backend/routes/index.js
+++ full-project/CET-backend/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const clienteController = require("../controllers/cliente.controller.js");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prueba backend CET' });
 });
+
+router.get('/comentarios',clienteController.allComentarios);
+router.get('/integrantes',clienteController.allIntegrantes);
+router.get('/tratamientos',clienteController.allTratamientos);
 
 module.exports = router;
