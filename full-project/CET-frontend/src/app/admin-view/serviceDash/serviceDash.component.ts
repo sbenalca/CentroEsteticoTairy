f760c81b23a25494c120cc84c1778f94c2a57ac0
sbenalca <37081244+sbenalca@users.noreply.github.com>
2021-02-03 02:57:26 -0500

    Consumo Api para Sección Usuarios y Servicio en Dashboard

--- full-project/CET-frontend/src/app/admin-view/serviceDash/serviceDash.component.ts
+++ full-project/CET-frontend/src/app/admin-view/serviceDash/serviceDash.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../../informacion.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-serviceDash',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./serviceDash.component.css']
 })
 export class ServiceDashComponent implements OnInit {
+  servicios;
+  tipos;
 
-  constructor() { }
+  constructor(private informacionService: InformacionService) { }
+
+  getTratamientos(): void{
+    const observablesList = [];
+    this.informacionService.getTratamientos().subscribe((data: any)=>{
+      this.servicios=data;
+      this.servicios.forEach(s => {
+        observablesList.push(this.informacionService.getTipo(s.idtiposervicio));
+      });
+      forkJoin(observablesList).subscribe((data:any) => {
+        this.tipos=data;
+      });
+    })
+  }
+
+
+  trackByOption(index, option) {
+    return option;
+  } 
 
   ngOnInit() {
+    this.getTratamientos();
   }
 
 }
--- full-project/CET-frontend/src/app/admin-view/usuarios/usuarios.component.ts
+++ full-project/CET-frontend/src/app/admin-view/usuarios/usuarios.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../../informacion.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+export class UsuariosComponent implements OnInit {
+  empleados;
+  integrantes;
+  areas;
+
+  constructor(private informacionService: InformacionService) {}
+
+  getUsuarios(): void{
+    const observablesListPersonas = [];
+    const observablesListAreas = [];
+    this.informacionService.getIntegrantes().subscribe((data: any)=>{
+      this.empleados=data;
+      this.empleados.forEach(em => {
+        observablesListPersonas.push(this.informacionService.getPersona(em.idpersona));
+        observablesListAreas.push(this.informacionService.getArea(em.idarea));
+      });
+      forkJoin(observablesListPersonas).subscribe((data:any) => {
+        this.integrantes=data;
+      });
+      forkJoin(observablesListAreas).subscribe((data:any) => {
+        this.areas=data;
+      });
+    });
+  }
+
+  trackByOption(index, option) {
+    return option;
+  }
 
   ngOnInit() {
+    this.getUsuarios();
   }
 
 }
