1cf024f65da48f9f0c992abc29bce1efd9e01398
Nicole Agila <nagila@espol.edu.ec>
2021-02-03 00:48:20 -0500

    send email

--- full-project/CET-frontend/src/app/app-routing.module.ts
+++ full-project/CET-frontend/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { EquipoComponent } from './equipo/equipo.component';
 import { PromocionesComponent } from './promociones/promociones.component';
 import { ServiciosComponent } from './servicios/servicios.component';
-import { DashboardComponent } from './admin-view/dashboard/dashboard.component';
 import { EstadisticaComponent } from './admin-view/estadistica/estadistica.component';
 import { CalendarioComponent } from './admin-view/calendario/calendario.component';
 import { LoginComponent } from './login/login.component';
@@ -17,8 +16,7 @@ const routes: Routes = [
   { path: "promociones", component: PromocionesComponent },
   { path: "servicios", component: ServiciosComponent },
   { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "estadistica", component: EstadisticaComponent },
+  { path: "dashboard", component: EstadisticaComponent },
   { path: "calendario", component: CalendarioComponent }
 ];
 
--- full-project/CET-frontend/src/app/contacto/contacto.component.ts
+++ full-project/CET-frontend/src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../informacion.service';
 
 @Component({
   selector: 'app-contacto',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  form = {
+    nombre:"",
+    fecha:"",
+    ciudad:"",
+    correo:"",
+    mensaje: ""
+  }
 
-  constructor() { }
+  constructor(private informacionService: InformacionService) { }
 
   ngOnInit() {
+  }
+
+  enviar(){
+    this.informacionService.postContactanos(this.form).subscribe((data: any)=>{
+    })
+
   }
 
 }
